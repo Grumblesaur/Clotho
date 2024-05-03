1bb8c3b67232586c48c1f8a5bc4d1a0772156be2
James Murphy <james.frank.murphy@proton.me>
2024-05-03 13:28:17 -0400

    resolved merge conflict with server ID passing

--- dicelang/script.py
+++ dicelang/script.py
@@ -2,28 +2,19 @@ import lark
 from dicelang.interpreter import DicelangInterpreter
 from dicelang.parser import parser
 from dicelang import result
-from dicelang.result import Result
 
 interpreter = DicelangInterpreter()
 
 
-def execute(owner: str, dicelang_script: str):
-    try:
-        ast = parser.parse(dicelang_script)
-    except lark.LarkError as e:
-        r = result.failure(error=e, console="[Syntax Error]")
-    else:
-        r = interpreter.execute(ast, as_owner=owner)
-    return r
 def execute(owner: str, server: str, dicelang_script: str):
     try:
         ast = parser.parse(dicelang_script)
-    except Exception as e:
-        result = Result(console="Parsing error. You may have mistyped a keyword, failed to put a string in quotes"
-                                " or misused an operator.", error=str(e).split('Expected')[0])
+    except lark.LarkError as e:
+        r = result.failure(error=e, console="Parsing error. You may have mistyped a keyword, failed "
+                                            "to put a string in quotes, or misused an operator.")
     else:
-        result = interpreter.execute(ast, as_owner=owner, on_server=server)
-    return result
+        r = interpreter.execute(ast, as_owner=owner, on_server=server)
+    return r
 
 
 if __name__ == '__main__':
